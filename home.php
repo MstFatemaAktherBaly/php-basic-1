a8567a84e2b0d12a4760b63ed8a191dc4e2cb29e
Mst Fatema Akther Baly <117789188+MstFatemaAktherBaly@users.noreply.github.com>
2023-12-03 08:26:18 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+
+
+<?php
+// variable
+$name = "Fatema";
+$she = "She";
+$age = 24;
+$hobby = "coding";
+
+echo "Hello world.<br>";
+
+echo " Mst $name Akther Baly is a Front End/Web Designer.<br>
+ $name was born in 27th september 1999.<br>
+ $she is a $age years old.<br>
+ $she loves $hobby in php programing language.<br> ";
+
+// constant
+
+ define('father_name', 'Md Belal Hossain');
+
+ echo father_name ;
+
+?>
+
+<!-- Not recommendable in php variable 
+1.numeric/numbers.
+2.reserve keyword not useable.
+3.any spesiphic charecter.
+4.any kind of emoji's. -->
